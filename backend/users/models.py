d681dd966d563f178a7babb4449316c52179a1aa
Сергей Корсаков <mortodello@mail.ru>
2024-03-28 15:42:16 +0300

    f

--- backend/users/models.py
+++ backend/users/models.py
@@ -32,9 +32,6 @@ class FoodgramUser(AbstractBaseUser, PermissionsMixin):
         verbose_name='Роль'
     )
     is_subscribed = models.BooleanField(default=False)
-    is_superuser = models.BooleanField(default=False)
-    is_staff = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
